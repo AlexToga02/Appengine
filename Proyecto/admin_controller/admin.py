fe71680462468364fecde2f54dcce0416fb3966e
Alejandro Toga <aotech10@gmail.com>
2015-11-13 16:40:31 -0600

    admin autorize

--- Proyecto/admin_controller/admin.py
+++ Proyecto/admin_controller/admin.py
@@ -3,7 +3,8 @@ import os
 import jinja2
 
 from webapp2_extras import sessions
-
+from apiclient.discovery import build
+from oauth2client.appengine import OAuth2Decorator
 
 template_dir = os.path.join(os.path.dirname(__file__), '..', 'templates')
 
@@ -11,6 +12,13 @@ template_dir = os.path.join(os.path.dirname(__file__), '..', 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
+#************ oauth2Decorator
+decorator = OAuth2Decorator(
+    client_id='141147046388-jekjfmorkjlhpt5eh9nh9rvuk3h443lv.apps.googleusercontent.com',
+    client_secret='foKPCKiZST5HloYvhGStc-iJ',
+    scope='https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/calendar')
+service = build('tasks','v1')
+service_calendar = build('calendar', 'v3')
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -40,8 +48,20 @@ class Handler(webapp2.RequestHandler):
         self.response.out.write(*a, **kw)
 
 class AdminHandler(Handler):
+    @decorator.oauth_required
     def get(self):
-        self.render("paginaadmin.html")
+         tasks=service.tasks().list(tasklist='@default').execute(http=decorator.http())
+         items = tasks.get('items', [])
+         notas = ','.join([task.get('title','') for task in items])
+         lista = notas.split(',')
+         numero = len(lista)
+
+        http=decorator.http()
+        request=service_calendar.events().list(calendarId='primary')
+        response_calendar=request.execute(http=http)
+        events =  response_calendar['items']
+
+        self.render("paginaadmin.html",eventos=events,items=items,num=numero)
 
 class Messageadmin(Handler):
     def get(self):
